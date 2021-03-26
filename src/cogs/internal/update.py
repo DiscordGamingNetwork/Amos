165834473811bf5072b8a326e4f0f06a5cb7d52d
vcokltfre <vcokltfre@gmail.com>
2021-03-26 19:01:41 +0000

    feat: add migrate command

--- src/cogs/internal/update.py
+++ src/cogs/internal/update.py
@@ -2,6 +2,7 @@ from discord import Embed
 from discord.ext import commands
 
 from os import system
+from pathlib import Path
 
 from src.internal.bot import Bot
 from src.internal.context import Context
@@ -14,8 +15,12 @@ class Update(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(name="update")
+    @commands.command(name="run")
     @commands.is_owner()
+    async def run(self, ctx: Context):
+        pass
+
+    @run.command(name="update")
     async def ping(self, ctx: Context):
         """Update the bot."""
 
@@ -25,6 +30,17 @@ class Update(commands.Cog):
         ))
 
         system("./update.sh")
+
+    @run.command(name="migrate")
+    async def migrate(self, ctx: Context, filename: str):
+        """Run a database migration."""
+
+        p = Path(f"./src/data/{filename}.sql")
+
+        with p.open() as f:
+            await self.bot.db.execute(f.read())
+
+        await ctx.reply(f"Successfully run migration: `{filename}`")
 
 
 def setup(bot: Bot):
